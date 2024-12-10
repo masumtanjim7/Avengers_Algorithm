31224b047ef21d0a61fc269636c586a7b4f6d242
SUBIR <111043450+sbsubir@users.noreply.github.com>
2024-12-10 19:53:59 +0600

    SUBIR

--- Group Projects - Avengers Algorithm/TrendyTreasure/admin/register_admin.php
+++ Group Projects - Avengers Algorithm/TrendyTreasure/admin/register_admin.php
@@ -12,6 +12,7 @@ if(!isset($admin_id)){
 
 if(isset($_POST['submit'])){
 
+   
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
    $pass = sha1($_POST['pass']);
@@ -46,12 +47,14 @@ if(isset($_POST['submit'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>register</title>
 
+   
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="../css/admin_style.css">
 
+   
 </head>
 <body>
 
