10202e1c47a706b54c247aa5222b08ca1b827506
SUBIR <subirmx@gmail.com>
2024-12-10 18:04:10 +0600

    SUBIR

--- Group Projects - Avengers Algorithm/TrendyTreasure/profile.php
+++ Group Projects - Avengers Algorithm/TrendyTreasure/profile.php
@@ -12,7 +12,6 @@ if(isset($_SESSION['user_id'])){
 };
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,11 +50,7 @@ if(isset($_SESSION['user_id'])){
 
 </section>
 
-
-
 <?php include 'components/footer.php'; ?>
-
-
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
